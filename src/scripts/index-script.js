43e401a142461f38c167ef3e0540b2fec39b6527
Csepreghy Tamás <csepytomi@gmail.com>
2024-11-07 22:08:19 +0100

    script for handling the sidebar in index.html

---
+++ src/scripts/index-script.js
@@ -0,0 +1,6 @@
+function toggleSidebar() {
+    const sidebar = document.getElementById("sidebar");
+    const content = document.querySelector(".content");
+    sidebar.classList.toggle("active");
+    content.classList.toggle("shift");
+}
